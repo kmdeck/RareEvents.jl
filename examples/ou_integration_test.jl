79488c58ae781e77eee6794a62d9a03cbd8ac951
kmdeck <kdeck@caltech.edu>
2022-09-22 09:25:26 -0700

    final version

--- examples/ou_integration_test.jl
+++ examples/ou_integration_test.jl
@@ -24,12 +24,12 @@ model = OrnsteinUhlenbeck{FT}(θ, σ, dimensions)
 
 # Set up rare event algorithm
 τ = 0.5
-nensemble = 500
+nensemble = 1000
 u0 = [copy(zeros(dimensions)) .+ randn(dimensions)*0.71 for i in 1:nensemble]
-k = 0.4
+k = 0.3
 ϵ = 0.002
 metric(y) = y[1]
-t_end = 200.0
+t_end = 50.0
 t_start = 0.0
 tspan = (t_start, t_end)
 # averaging window
@@ -37,7 +37,7 @@ T = 50
 steps_per_window = Int64.(round(T/dt))
 
 
-# Run GKLT -  bootstrap iterations - and compute necessary statistics per iteration
+# Run GKLT
 iters = 5
 em = zeros(nensemble*iters)
 em = reshape(em, (nensemble,iters))
@@ -60,7 +60,7 @@ for iter in 1:iters
     for i in 1:nensemble
         trajectory = [metric(sim.ensemble[i][j]) for j in 1:length(sim.ensemble[i])]
         lr_vector[i] = likelihood_ratio(sim, trajectory, λ, t_end-t_start)
-        # This just counts the number of (treated as independent) block means per bin, over all trajectories, over all iterations
+        # This just counts the number of (treated as independent) block means per bin, over all trajectories, over all iterations. Not doing anything with it, yet
         counts .+=  count_events_per_bin(block_mean(trajectory, steps_per_window), lr_vector[i],a_range)
         # This is for estimating return times via the Ragone paper method; via `return_curve`.
         a_m[i] = maximum(block_mean(trajectory, steps_per_window))
@@ -73,7 +73,7 @@ rmprocs([p for p in procs() if p != myid()])
 
 
 # Evolve the solution directly; compute statistics
-tspan_direct = (0.0, 5e5)
+tspan_direct = (0.0, 2.5e5)
 direct_solution = evolve_stochastic_system(model, zeros(dimensions), tspan_direct, dt, (maxiters = 1e8,));
 direct_cost = (tspan_direct[2]-tspan_direct[1])*dt
 u = [metric(direct_solution[k]) for k in 1:length(direct_solution)]
@@ -97,8 +97,7 @@ upper_curve = [sort(gev_rtn[:,k])[83] for k in 1:length(a_range)]
 lower_curve = [sort(gev_rtn[:,k])[17] for k in 1:length(a_range)]
 
 
-# Make plots
-plot1 = plot()
+# GKLT curve - we need to combine iterations into single result
 mean_a = zeros(length(a_range)-1)
 mean_rtn = zeros(length(a_range)-1)
 std_rtn = zeros(length(a_range)-1)
@@ -106,25 +105,24 @@ for i in 1:(length(a_range)-1)
     mask = (em[:] .< a_range[i+1]) .& ( em[:] .>= a_range[i])
     if sum(mask) >0
         mean_a[i] = mean(em[:][mask])
-        mean_rtn[i] = mean(r_paper[:][mask])#log10.(mean(r_paper[:][mask]))
-        std_rtn[i] = std(r_paper[:][mask])#/mean(r_paper[:][mask])./log(10.0)
+        mean_rtn[i] = mean(r_paper[:][mask])
+        std_rtn[i] = std(r_paper[:][mask])
     end
 end
 nonzero = (mean_a .!= 0.0) .& (isnan.(std_rtn) .== 0)
 final_a = mean_a[nonzero]
 final_r = mean_rtn[nonzero]
 final_σr = std_rtn[nonzero]
-plot!(plot1,final_a, final_r, ribbon = (min.(final_r .- 1, final_σr), final_σr), label = "GKLT Algorithm")
 
-#denominator = 1.0 .- cumsum(counts)/sum(counts)
-#safe_denominator = denominator .> 10.0*eps(FT)
-#plot!(plot1,a_range[safe_denominator], log10.(50.0 ./denominator[safe_denominator]), label = "GKLT Algorithm Counts")
-plot!(plot1,em_direct, r_paper_direct, ribbon = (min.(r_paper_direct .- 1, σr_direct), σr_direct), label = "Direct Integration")
-plot!(plot1, a_range, median_curve, ribbon = (min.(median_curve .-1, median_curve .- lower_curve), upper_curve .- median_curve), label = "GEV", color = :purple)
+plot1 = plot()
+plot!(plot1,final_a, final_r, ribbon = final_σr, label = "GKLT Algorithm")
+plot!(plot1,em_direct[2:end], r_paper_direct[2:end], ribbon = σr_direct[2:end], label = "Direct Integration")
+plot!(plot1, a_range, median_curve, ribbon = (median_curve .- lower_curve, upper_curve .- median_curve), label = "GEV", color = :purple)
 plot!(plot1,xlabel = "Event magnitude")
 plot!(plot1,ylabel = "Return Time")
 plot!(plot1, legend = :topleft)
 plot!(plot1, xrange = [0.0,0.8])
 plot!(plot1, yaxis = :log)
-plot!(plot1, yrange = (1,1e10)
-savefig(plot1, "return_curve_ou_tmp.png")
+plot!(plot1, yrange = (1,1e10), yticks = [1, 1e2, 1e4, 1e6, 1e8, 1e10])
+savefig(plot1, "return_curve_ou.png")
+@assert direct_cost ≈ algorithm_cost
