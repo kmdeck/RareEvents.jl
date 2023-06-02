c5ee99c33bde444566b98121cb40d481cf76d432
kmdeck <kdeck@caltech.edu>
2023-06-01 18:41:50 -0700

    it runs

--- examples/grf/sim.jl
+++ examples/grf/sim.jl
@@ -1,5 +1,7 @@
 using DifferentialEquations: CorrelatedWienerProcess!, SDEProblem, solve
 using Plots
+using StatsBase
+
 
 function convert_to_animation(x, time_stride, N, clims)
     init_frames = length(x)
@@ -17,6 +19,11 @@ end
 abstract type AbstractBoundaryConditions end
 
 struct Periodic <: AbstractBoundaryConditions end
+struct Dirichlet{FT} <: AbstractBoundaryConditions
+    bc::FT
+end
+
+
 
 struct LinearDiffusionSDE{FT <:AbstractFloat, BC}
     σ::FT
@@ -53,7 +60,6 @@ function stochastic_system(model::LinearDiffusionSDE{FT, Periodic}) where {FT}
                 end
             end
         end
-
     end
     
     function stochastic_tendency!(du,u,p,t)
@@ -64,14 +70,51 @@ function stochastic_system(model::LinearDiffusionSDE{FT, Periodic}) where {FT}
 end
 
 
-function evolve_stochastic_system(model::LinearDiffusionSDE, u0, tspan, dt, alg_kwargs)
+function stochastic_system(model::LinearDiffusionSDE{FT, Dirichlet{FT}}) where {FT}
+    function deterministic_tendency!(du,u,p,t)
+        N = p.N
+        du .= 0
+        u_bc = p.u_bc
+        for i in 1:N
+            for j in 1:N
+                k = (j-1)*N+i
+                k_ip1 = k+1
+                k_im1 = k-1
+                k_jp1 = k + N
+                k_jm1 = k - N  
+                if i == N          
+                    du[k] += 1/2*((u[k_im1] - u[k]) - (u[k] - u_bc))
+                elseif i ==1
+                    du[k] += 1/2*((u_bc - u[k]) - (u[k] - u[k_ip1]))
+                else
+                    du[k] += 1/2*((u[k_im1] - u[k]) - (u[k] - u[k_ip1]))
+                end
+                if j == N
+                    du[k] += 1/2*((u_bc - u[k]) - (u[k] - u[k_jm1]))
+                elseif j ==1
+                    du[k] += 1/2*((u[k_jp1] - u[k]) - (u[k] - u_bc))
+                else
+                    du[k] += 1/2*((u[k_jp1] - u[k]) - (u[k] - u[k_jm1]))
+                end
+            end
+        end
+    end
+    
+    function stochastic_tendency!(du,u,p,t)
+        du .= p.σ
+    end
+    p = (u_bc = model.bc.bc, σ = model.σ, model.N)
+    return deterministic_tendency!, stochastic_tendency!, p
+end
+
+
+function evolve_stochastic_system(model::LinearDiffusionSDE, u0, tspan, saveat, dt, alg_kwargs)
     deterministic_tendency!, stochastic_tendency!, p = stochastic_system(model)
     Γ = model.Γ
-#    W0 = reshape(randn(model.N^4)*dt, (model.N^2, model.N^2))
     dW = CorrelatedWienerProcess!(Γ, tspan[1], zeros(model.N^2), zeros(model.N^2))
     prob = SDEProblem(deterministic_tendency!, stochastic_tendency!, u0, tspan, p; noise = dW)
-    sol = solve(prob, dt = dt, saveat = tspan[1]:dt:tspan[2]; alg_kwargs...);
-    return sol.u
+    sol = solve(prob, dt = dt, saveat = saveat; alg_kwargs...);
+    return sol.t, sol.u
 end
 N = 32
 Γ = reshape(zeros(N^4), (N^2,N^2))
@@ -86,7 +129,7 @@ for i1 in 1:N
                 if k1 == k2
                     Γ[k1,k2] = 1.0
                 else
-                    Γ[k1,k2] = 1.0/sqrt((i1-i2)^2 + (j1-j2)^2)#cos((i1-i2)*2*pi/4)*cos((j1-j2)*2*pi/4)
+                    Γ[k1,k2] = 1.0/sqrt((i1-i2)^2 + (j1-j2)^2)
                 end
                 
             end
@@ -95,12 +138,35 @@ for i1 in 1:N
 end
 
            
-model = LinearDiffusionSDE(0.1, Γ, N, Periodic())
-
-tspan = (0.0,40.0)
+#model = LinearDiffusionSDE(1.0, Γ, N, Periodic())
+model = LinearDiffusionSDE(1.0, Γ, N, Dirichlet(0.0))
+# TODO: Test that a smaller timestep gives the same results.
+# Tested at 3e4 originally
+# TODO: Run for approximately 
+tspan = (0.0,2e6)
 dt = 0.1
+T = 50
+n = Int(T/dt)
+saveat = tspan[1]:dt*n:tspan[2]
 alg_kwargs = (adaptive=false,)
-sol = evolve_stochastic_system(model, u0, tspan, dt, alg_kwargs)
-clims= (minimum(minimum(sol[:])),maximum(maximum(sol[:])))
-anim = convert_to_animation(sol, 1, N, clims)
-gif(anim, "anim.gif", fps = 20)
+t, u = evolve_stochastic_system(model, u0, tspan, saveat, dt, alg_kwargs);
+# autocorrelation time
+center_row = Int64.((Array(1:1:N) .-1) .*N .+N/2)
+values = zeros(length(u), N)
+for i in 1:length(u)
+    values[i,:] .= u[i][center_row]
+end
+p528 = [u[k][528] for k in 1:length(t)]
+Plots.plot(t, p528)
+lags = Array(1:1:(length(t)-1)) # in units of time
+ac = StatsBase.autocor(values, lags; demean = true)
+mean_ac = mean(ac, dims = 2)[:]
+
+Plots.plot(lags, mean(ac, dims = 2)[:])
+# by eye, a lag of lags[3000]*dt = 3e2 is the autocorrelation time
+τ = maximum(lags[abs.(mean_ac) .> 0.1])*T
+n_τ = (tspan[2]-tspan[1])/τ
+
+#clims= (minimum(minimum(sol.u[:])),maximum(maximum(sol.u[:])))
+#anim = convert_to_animation(sol, 300, N, clims)
+#gif(anim, "anim.gif", fps = 10)
